f09f6cb980bc831efd717cb63e113e3f3e8b419d
ZabirAlam <zabiralam7866@gmail.com>
2026-08-14 23:17:17 +0530

    Add files via upload

---
+++ circles.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,r,area;
+	cout<<"Enter radius=";
+	cin>>r;
+	
+	area=3.14*r*r;
+	
+	cout<<"area of circle="<<area;
+	return 0;
+	
+}
+
